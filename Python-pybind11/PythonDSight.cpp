5ae71aa1ba68cc12d6fb2554f7e2f752ae5c7824
denissalem <denissalem@tuxfamily.org>
2019-01-23 19:12:32 +0100

    Get rid of pybind11

--- Python-pybind11/PythonDSight.cpp
+++
@@ -1,9 +0,0 @@
-#include "PythonDSight.hpp"
-
-PYBIND11_MODULE(dsight, m) {
-	InitEnum(m);
-	InitExceptions(m);
-	InitCharts(m);
-	InitContextHandler(m);
-	m.def("create_dict", &create_dict, py::return_value_policy::reference);
-}
--- Python-pybind11/PythonExceptions.cpp
+++
@@ -1,9 +0,0 @@
-#include <pybind11/pybind11.h>
-
-#include "Exceptions/DSightBaseException.hpp"
-
-namespace py = pybind11;
-
-void InitExceptions(py::module &m) {
-	py::register_exception<DSight::DSightBaseException>(m, "DSightBaseException");
-}
